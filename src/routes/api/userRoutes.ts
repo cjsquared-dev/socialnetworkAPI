7fc2b8f3694802c4005fb53237856a16572c8d49
cjsquared-dev <camjohnson0618@gmail.com>
2024-12-13 14:30:08 -0700

    updated delete user routes

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
-import { createUser, getUsers, getSingleUser } from '../../controllers/userController.js';
+import { createUser, getUsers, getSingleUser, deleteUser } from '../../controllers/userController.js';
 
 router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getSingleUser);
+router.route('/:userId').delete(deleteUser);
 
 
 
