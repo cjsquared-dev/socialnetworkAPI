0b1857b28800e478cea896006352c66082bfed05
cjsquared-dev <camjohnson0618@gmail.com>
2024-12-13 14:33:18 -0700

    updated updateUser routes in API routes

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 const router = Router();
-import { createUser, getUsers, getSingleUser, deleteUser } from '../../controllers/userController.js';
+import { createUser, getUsers, getSingleUser, deleteUser, updateUser } from '../../controllers/userController.js';
 
 router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getSingleUser);
 router.route('/:userId').delete(deleteUser);
+router.route('/:userId').put(updateUser);
+
 
 
 
